d39bd69b1e37eacb1c87e3dae4a4ac283ddc874a
dohyeon-kinn <dohyeon.kim@gluwa.com>
2025-04-09 14:00:00 +0900

    add fail case

--- src/App.jsx
+++ src/App.jsx
@@ -10,40 +10,44 @@ import { wordlist as english } from '@scure/bip39/wordlists/english';
 function App() {
   useEffect(() => {
     (async () => {
-      await cryptoWaitReady();
+      try {
+        await cryptoWaitReady();
         
-      let addressData = {
-        evm: {},
-        substrate: {}
-      };
-      const mnemonic = bip39.generateMnemonic(english);
+        let addressData = {
+          evm: {},
+          substrate: {}
+        };
+        const mnemonic = bip39.generateMnemonic(english);
 
-      const seed = bip39.mnemonicToSeedSync(mnemonic);
-      const hdWallet = hdkey.EthereumHDKey.fromMasterSeed(seed);
-      const wallet = hdWallet.derivePath("m/44'/60'/0'/0/0").getWallet();
+        const seed = bip39.mnemonicToSeedSync(mnemonic);
+        const hdWallet = hdkey.EthereumHDKey.fromMasterSeed(seed);
+        const wallet = hdWallet.derivePath("m/44'/60'/0'/0/0").getWallet();
 
-      const ethAddress = wallet.getAddressString();
-      const ethPrivateKey = wallet.getPrivateKeyString();
+        const ethAddress = wallet.getAddressString();
+        const ethPrivateKey = wallet.getPrivateKeyString();
 
-      addressData.evm = {
-        address: ethAddress,
-        public: '',
-        private: ethPrivateKey,
-        index: 0,
+        addressData.evm = {
+          address: ethAddress,
+          public: '',
+          private: ethPrivateKey,
+          index: 0,
+        }
+
+        const keyring = new Keyring({ type: 'sr25519', ss58Format: 42 });
+        const polkadotAccount = keyring.createFromUri(mnemonic, { name: 'sr25519' });
+        const polkadotPrivateKey = u8aToHex(mnemonicToMiniSecret(mnemonic));
+
+
+        addressData.substrate = {
+          address: polkadotAccount.address,
+          public: Array.from(polkadotAccount.publicKey),
+          private: polkadotPrivateKey,
+        }
+
+        window?.ReactNativeWebView?.postMessage(JSON.stringify({ addresses: addressData, mnemonic: mnemonic }));
+      } catch {
+        window?.ReactNativeWebView?.postMessage(JSON.stringify({ addresses: null, mnemonic: null }));
       }
-
-      const keyring = new Keyring({ type: 'sr25519', ss58Format: 42 });
-      const polkadotAccount = keyring.createFromUri(mnemonic, { name: 'sr25519' });
-      const polkadotPrivateKey = u8aToHex(mnemonicToMiniSecret(mnemonic));
-
-
-      addressData.substrate = {
-        address: polkadotAccount.address,
-        public: Array.from(polkadotAccount.publicKey),
-        private: polkadotPrivateKey,
-      }
-
-      window?.ReactNativeWebView?.postMessage(JSON.stringify({addresses: addressData, mnemonic: mnemonic}));
     })()  
   }, []);
 
